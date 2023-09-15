5e6b7c5b34074d51d91e093031f4f1a9a8874fd1
PratikYavatkar <pyavatkar@gmail.com>
2023-09-15 19:00:14 +0530

    added iam

---
+++ Terraform/EC2/ec2.tf
@@ -0,0 +1,8 @@
+resource "aws_instance" "instace1" {
+  ami                     = "ami-05552d2dcf89c9b24"
+  instance_type           = "t2.micro"
+  iam_instance_profile = ""
+  tags = {
+    name = "Terraform instance 1"
+  }
+}
