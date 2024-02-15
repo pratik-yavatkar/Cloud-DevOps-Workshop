fe17f5bbca35da17dd2469c89396e7f10a05bb79
Pratik Yavatkar <118264284+pratik-yavatkar@users.noreply.github.com>
2024-02-16 00:01:51 +0530

    Update ec2.tf

--- Terraform/EC2/ec2.tf
+++ Terraform/EC2/ec2.tf
@@ -3,6 +3,6 @@ resource "aws_instance" "instace1" {
   instance_type           = "t2.micro"
   iam_instance_profile = ""
   tags = {
-    name = "Terraform instance 1"
+    Name = "Terraform instance 1"
   }
 }
